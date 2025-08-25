ec2364e760350d9456dabc20ba692b51843da6c0
Davi Marques <davimarques22558800@gmail.com>
2025-08-25 17:00:59 -0300

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import AddTask from './components/AddTask';
+import Tasks from './components/Tasks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: 'Estudar React',
+    description: 'Tornar-se proficiente em React',
+    isCompleted: false
+  }, {
+    id: 2,
+    title: 'Estudar Node.js',
+    description: 'Aprender a construir APIs com Node.js', 
+    isCompleted: false
+  }, {
+    id: 3,
+    title: 'Estudar TypeScript',
+    description: 'Adicionar tipagem ao JavaScript com TypeScript',
+    isCompleted: false
+  }, ])
+
+
+function onTaskClick(taskId)  {
+  const newTasks = tasks.map(task => {
+    //Preciso atualizar essa tarefa
+    if (task.id === taskId) {
+      return {...task, isCompleted: !task.isCompleted
+      }
+    }
+
+    //Não preciso atualizar essa tarefa
+    return task
+  });
+
+  setTasks(newTasks);
+
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
+
+        <div className='w-[500px]'>
+
+          <h1 className='text-3xl text-slate-100 font-bold text-center'>
+            Gerenciador de Tarefas
+          </h1>
+
+          <AddTask />
+          <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+
+        </div>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,30 @@
+import { ChevronRight, ChevronRightIcon } from "lucide-react";
+
+function Tasks (props) { //Passar valores para outra parte do código
+    return (
+    <ul className="space-y-4 p-6 bg-slate-200 shadow">
+        {props.tasks.map((task) => (
+            <li key={task.id} className="flex gap-2">
+
+                <button 
+                    onClick={() => props.onTaskClick(task.id)}
+                    className={`bg-slate-400 text-left text-white p-2 rounded-md w-full 
+                        ${task.isCompleted && 'line-through'
+                    }`}
+                    >
+                    {task.title}
+
+
+                </button>
+
+                <button className="bg-slate-400 p-2 rounded-md text-white">
+                    <ChevronRightIcon />
+                </button>
+            
+            </li>
+        ))}
+    </ul>
+    )
+}
+
+export default Tasks;
