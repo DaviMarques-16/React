3e9d286605a550f4591740eb563b11d5f122dd26
Davi Marques <davimarques22558800@gmail.com>
2025-08-25 13:47:41 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-function App() {
-  //Isso é um componente: jsx
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-  //State ou estado:
-  let message = "Showman"; //let pq o valor pode mudar
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    //Retorna um único elemento: ex: uma div
-    <div>
-      <h2>{message}</h2>
-      <button>Mudar mensagem</button>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//Imports 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-//Inserção de conteúdo no HTML por meio do JS.
